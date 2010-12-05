92312f7ed12e4d23a04fbb165e18c4c483a29a5b
pontuyo <kobari1984@gmail.com>
2010-12-05 19:24:28 +0900

    タクトモ退会コメント

--- takutomo/apps/frontend/modules/member/templates/indexSuccess.php
+++ takutomo/apps/frontend/modules/member/templates/indexSuccess.php
@@ -118,5 +118,6 @@ foreach($get_attend_event_list as $key=>$value){
 <br />-->
 <?php echo sfJpMobile::getEmoji()->convert('&#xE6D3;');//メールの文字?>
 <a href="?guid=ON&amp;url=<?php echo sfConfig::get('sf_mixi_inquiry_url')?>">お問い合わせ</a>
-<hr />
+<!--<hr />
 <a href="?guid=ON&amp;url=<?php echo sfConfig::get('sf_mixi_delete_user_url')?>">ﾀｸﾄﾓ退会</a>
+-->
