db7b7aaf5d6007cbce12cf059ae25e8485ea8dc0
pontuyo <kobari1984@gmail.com>
2010-12-06 13:17:56 +0900

    privacyモジュール追加

---
+++ takutomo/apps/frontend/modules/privacy/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * privacy actions.
+ *
+ * @package    takutomo
+ * @subpackage privacy
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class privacyActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    
+  }
+  
+  public function executeIndividual(sfWebRequest $request)
+  {
+  	
+  }
+}
---
+++ takutomo/apps/frontend/modules/privacy/templates/individualSuccess.php
@@ -0,0 +1,7 @@
+<div style="text-align: left; background-color:#00ccff; color:#FFFFFF; margin:5px 1px; padding-top:2px;padding-left:2px;">
+<span sytle="font-size:small;">※ご入力いただいた個人情報の取り扱いについて</span>
+</div>
+このｱﾌﾟﾘで入力いただいたすべての情報は、株式会社ミクシィではなく株式会社スカイミントにより管理されます。
+株式会社スカイミントが定める<a href="?guid=ON&amp;url=<?php echo sfConfig::get('sf_mixi_privacy_url')?>">ﾌﾟﾗｲﾊﾞｼーﾎﾟﾘｼー</a>に同意の上、ご利用ください。
+入力頂いた情報は株式会社スカイミントでは一切保存致しません。
+入力情報は本ｱﾌﾟﾘ提供の目的のみに利用致します。
